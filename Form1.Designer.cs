d3287399483d7a0ee92a2ca3883f3ba55a92f843
Janis Simansons <valisjs@inbox.lv>
2020-05-16 15:31:37 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace PacManLv
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Klucis = new System.Windows.Forms.PictureBox();
+            this.Food = new System.Windows.Forms.PictureBox();
+            this.Enemy = new System.Windows.Forms.PictureBox();
+            this.TimerKlucisMove = new System.Windows.Forms.Timer(this.components);
+            this.TimerKlucisAnimate = new System.Windows.Forms.Timer(this.components);
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.TimerEnemyAnimate = new System.Windows.Forms.Timer(this.components);
+            this.TimerKlucisMelt = new System.Windows.Forms.Timer(this.components);
+            this.TimerEnemyMove = new System.Windows.Forms.Timer(this.components);
+            this.GameOverLabel = new System.Windows.Forms.Label();
+            this.RestartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Klucis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Food)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Klucis
+            // 
+            this.Klucis.BackColor = System.Drawing.Color.Lime;
+            this.Klucis.Location = new System.Drawing.Point(133, 68);
+            this.Klucis.Name = "Klucis";
+            this.Klucis.Size = new System.Drawing.Size(100, 100);
+            this.Klucis.TabIndex = 0;
+            this.Klucis.TabStop = false;
+            // 
+            // Food
+            // 
+            this.Food.BackColor = System.Drawing.Color.Red;
+            this.Food.Location = new System.Drawing.Point(377, 138);
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(30, 30);
+            this.Food.TabIndex = 1;
+            this.Food.TabStop = false;
+            // 
+            // Enemy
+            // 
+            this.Enemy.BackColor = System.Drawing.Color.Fuchsia;
+            this.Enemy.Location = new System.Drawing.Point(554, 138);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(50, 50);
+            this.Enemy.TabIndex = 2;
+            this.Enemy.TabStop = false;
+            // 
+            // TimerKlucisMove
+            // 
+            this.TimerKlucisMove.Interval = 20;
+            this.TimerKlucisMove.Tick += new System.EventHandler(this.TimerKlucisMove_Tick);
+            // 
+            // TimerKlucisAnimate
+            // 
+            this.TimerKlucisAnimate.Tick += new System.EventHandler(this.TimerKlucisAnimate_Tick);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.ScoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(72, 30);
+            this.ScoreLabel.TabIndex = 3;
+            this.ScoreLabel.Text = "Score";
+            // 
+            // TimerEnemyAnimate
+            // 
+            this.TimerEnemyAnimate.Tick += new System.EventHandler(this.TimerEnemyAnimate_Tick);
+            // 
+            // TimerKlucisMelt
+            // 
+            this.TimerKlucisMelt.Tick += new System.EventHandler(this.TimerKlucisMelt_Tick);
+            // 
+            // TimerEnemyMove
+            // 
+            this.TimerEnemyMove.Interval = 20;
+            this.TimerEnemyMove.Tick += new System.EventHandler(this.TimerEnemyMove_Tick);
+            // 
+            // GameOverLabel
+            // 
+            this.GameOverLabel.AutoSize = true;
+            this.GameOverLabel.Font = new System.Drawing.Font("Constantia", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.GameOverLabel.Location = new System.Drawing.Point(288, 93);
+            this.GameOverLabel.Name = "GameOverLabel";
+            this.GameOverLabel.Size = new System.Drawing.Size(260, 59);
+            this.GameOverLabel.TabIndex = 4;
+            this.GameOverLabel.Text = "Game Over";
+            this.GameOverLabel.Visible = false;
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.RestartButton.Location = new System.Drawing.Point(612, 207);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(150, 50);
+            this.RestartButton.TabIndex = 5;
+            this.RestartButton.Text = "RESTART";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Visible = false;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.GameOverLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.Food);
+            this.Controls.Add(this.Klucis);
+            this.Name = "Game";
+            this.Text = "7";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Klucis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Food)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Klucis;
+        private System.Windows.Forms.PictureBox Food;
+        private System.Windows.Forms.PictureBox Enemy;
+        private System.Windows.Forms.Timer TimerKlucisMove;
+        private System.Windows.Forms.Timer TimerKlucisAnimate;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Timer TimerEnemyAnimate;
+        private System.Windows.Forms.Timer TimerKlucisMelt;
+        private System.Windows.Forms.Timer TimerEnemyMove;
+        private System.Windows.Forms.Label GameOverLabel;
+        private System.Windows.Forms.Button RestartButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacManLv
+{
+    public partial class Game : Form
+    {
+        int klucisStep = 5;
+        int enemyStep = 3;
+        int verVelocity = 0;
+        int horVelocity = 0;
+        int klucisImage = 1;
+        int enemyImage = 1;
+        int score = 0;
+        int verEnemyVelocity = 0;
+        int horEnemyVelocity = 0;
+        string klucisDirection = "right";
+        string enemyDirection = "down";
+        Random Rand = new Random();
+
+        public Game()
+        {
+            InitializeComponent();
+            SetupGame();
+        }
+
+        private void SetRandomEnemyDirection()
+        {
+            int directionCode = Rand.Next(1, 5);
+            if (directionCode == 1)
+            {
+                enemyDirection = "right";
+                verEnemyVelocity = 0;
+                horEnemyVelocity = enemyStep;
+            }
+            else if (directionCode == 2)
+            {
+                enemyDirection = "down";
+                verEnemyVelocity = enemyStep;
+                horEnemyVelocity = 0;
+            }
+            else if (directionCode == 3)
+            {
+                enemyDirection = "left";
+                verEnemyVelocity = 0;
+                horEnemyVelocity = -enemyStep;
+
+            }
+            else if (directionCode == 4)
+            {
+                enemyDirection = "up";
+                verEnemyVelocity = -enemyStep;
+                horEnemyVelocity = 0;
+            }
+        }
+
+        private void SetupGame()
+        {
+            Klucis.BackColor = Color.Transparent;
+            Klucis.SizeMode = PictureBoxSizeMode.StretchImage;
+            Klucis.Width = 50;
+            Klucis.Height = 50;
+            Food.BackColor = Color.Green;
+            Food.BackColor = Color.Transparent;
+            Food.Image = Properties.Resources.food_3;
+            Food.SizeMode = PictureBoxSizeMode.StretchImage;
+            Enemy.BackColor = Color.Transparent;
+            Enemy.SizeMode = PictureBoxSizeMode.StretchImage;
+            Enemy.Width = 40;
+            Enemy.Height = 40;
+            this.BackColor = Color.Blue;
+
+            SetRandomEnemyDirection();
+
+            UpdateScoreLabel();
+
+            //initializing timers
+            TimerKlucisMove.Start();
+            TimerKlucisAnimate.Start();
+            TimerEnemyAnimate.Start();
+            TimerEnemyMove.Start();
+            
+        }
+
+        private void KlucisFoodCollision()
+        {
+            if (Klucis.Bounds.IntersectsWith(Food.Bounds))
+            {
+                score += 100;
+                RandomizeFood();
+                UpdateScoreLabel();
+            }
+        }
+
+        private void UpdateScoreLabel()
+        {
+            ScoreLabel.Text = "Score: " + score;
+        }
+
+        private void RandomizeFood()
+        {
+            Food.Top = Rand.Next(0, ClientRectangle.Height - Food.Height);
+            Food.Left = Rand.Next(0, ClientRectangle.Width - Food.Width);
+            Food.Image = (Image)Properties.Resources.ResourceManager.GetObject("food_" + Rand.Next(1, 5));           
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {                
+                verVelocity = -klucisStep;
+                horVelocity = 0;
+                klucisDirection = "up";
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                verVelocity = klucisStep;
+                horVelocity = 0;
+                klucisDirection = "down";
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                verVelocity = 0;
+                horVelocity = -klucisStep;
+                klucisDirection = "left";
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                verVelocity = 0;
+                horVelocity = klucisStep;
+                klucisDirection = "right";
+            }
+            SetRandomEnemyDirection();
+        }
+
+        private void KlucisBorderCollision()
+        {
+            if (Klucis.Top + Klucis.Height < 0)
+            {
+                Klucis.Top = ClientRectangle.Height;
+            }
+            if (Klucis.Top > ClientRectangle.Height)
+            {
+                Klucis.Top = 0 - Klucis.Height;
+            }
+            if (Klucis.Left + Klucis.Width < 0)
+            {
+                Klucis.Left = ClientRectangle.Width;
+            }
+            if (Klucis.Left > ClientRectangle.Width)
+            {
+                Klucis.Left = 0 - Klucis.Width;
+            }
+        }
+
+        private void EnemyBorderColision()
+        {
+            if (Enemy.Top < 0)
+            {
+                verEnemyVelocity = enemyStep;
+            }
+            if (Enemy.Top > ClientRectangle.Height - Enemy.Height)
+            {
+                verEnemyVelocity = -enemyStep;
+            }
+            if (Enemy.Left < 0)
+            {
+                horEnemyVelocity = enemyStep;
+            }
+            if (Enemy.Left > ClientRectangle.Width - Enemy.Width)
+            {
+                horEnemyVelocity = -enemyStep;
+            }
+        }
+
+        private void KlucisEnemyCollision()
+        {
+            if (Klucis.Bounds.IntersectsWith(Enemy.Bounds))
+            {
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            TimerKlucisMove.Stop();
+            TimerKlucisAnimate.Stop();
+            TimerEnemyAnimate.Stop();
+            TimerEnemyMove.Stop();
+            Enemy.Visible = false;
+            Food.Visible = false;
+            klucisImage = 0;
+            TimerKlucisMelt.Start();
+        }
+
+        private void TimerKlucisMove_Tick(object sender, EventArgs e)
+        {
+            Klucis.Top += verVelocity;
+            Klucis.Left += horVelocity;
+            KlucisBorderCollision();
+            KlucisFoodCollision();
+            KlucisEnemyCollision();
+        }
+
+        private void TimerKlucisAnimate_Tick(object sender, EventArgs e)
+        {
+            string klucisImageName;
+            klucisImageName = "pacman_" + klucisDirection + "_" + klucisImage;
+            Klucis.Image = (Image)Properties.Resources.ResourceManager.GetObject(klucisImageName);
+            klucisImage += 1;
+            if (klucisImage > 4)
+            {
+                klucisImage = 1;
+            }
+        }
+
+        private void TimerEnemyAnimate_Tick(object sender, EventArgs e)
+        {
+            string enemyImageName;
+            enemyImageName = "enemy_" + enemyDirection + "_" + enemyImage;
+            Enemy.Image = (Image)Properties.Resources.ResourceManager.GetObject(enemyImageName);
+            enemyImage += 1;
+            if (enemyImage > 2)
+            {
+                enemyImage = 1;
+            }
+        }
+
+        private void TimerKlucisMelt_Tick(object sender, EventArgs e)
+        {
+            string klucisImageName;
+            klucisImageName = "pacman_melt_" + klucisImage;
+            Klucis.Image = (Image)Properties.Resources.ResourceManager.GetObject(klucisImageName);
+            klucisImage += 1;
+            if (klucisImage > 14)
+            {
+                TimerKlucisMelt.Stop();
+                GameOverLabel.Visible = true;
+                RestartButton.Visible = true;                
+            }
+        }
+
+        private void TimerEnemyMove_Tick(object sender, EventArgs e)
+        {
+            Enemy.Top += verEnemyVelocity;
+            Enemy.Left += horEnemyVelocity;
+            EnemyBorderColision();
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
